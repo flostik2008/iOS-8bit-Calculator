ddac250dcd52b04f870a11a24b65e592e1a9ef90
Evgeny <flostik2008@gmail.com>
2016-08-03 21:09:52 -0700

    Initial Commit

---
+++ S3L40retro-calulator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  S3L40retro-calulator
+//
+//  Created by Evgeny on 8/3/16.
+//  Copyright © 2016 Evgeny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
